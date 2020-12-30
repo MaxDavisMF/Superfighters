28fa05317098f6b9bdba9b19209b440c2ab8eb3c
MaxDavisMF <64198116+MaxDavisMF@users.noreply.github.com>
2020-12-30 23:52:38 +0000

    Update 1 - Player class, Movement and animation attempt
    Added player class and movement controls. Tried to add animation for walking (in the move subroutine for the player class) but am struggling to get it working due to each sprite in the animation being different sizes. Approached animation by cycling through an array of images when the character is walking

--- Superfighters.py
+++ Superfighters.py
@@ -10,12 +10,56 @@ RED = (255, 0, 0)
  
 pygame.init()
  
-# Set the width and height of the screen [width, height]
+# Set the screen up
 size = (1000, 750)
 screen = pygame.display.set_mode(size)
- 
 pygame.display.set_caption("Superfighters")
- 
+
+#Images
+player_still_image= pygame.image.load("standright_1.png").convert()
+player_walk_right1 = pygame.image.load("walkright_1.png").convert()
+player_walk_right1 = pygame.transform.scale(player_walk_right1, (42, 60))
+#player_walk_right2 = pygame.image.load("walkright_2.png").convert()
+#player_walk_right2 = pygame.transform.scale(player_walk_right2, (27, 60))
+player_walk_right3 = pygame.image.load("walkright_3.png").convert()
+player_walk_right3 = pygame.transform.scale(player_walk_right3, (48, 60))
+#player_walk_right4 = pygame.image.load("walkright_4.png").convert()
+
+#create lists to cycle through for animations
+walkright = [player_walk_right1, player_walk_right3]
+#Classes
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = player_still_image
+        self.image.set_colorkey(BLACK)
+        self.image = pygame.transform.scale(player_still_image, (37, 63))
+        self.rect = self.image.get_rect()
+        self.rect.x = 45
+        self.rect.y = 15
+        self.walkanimationnum = 0
+
+    def move(self, speed, timer):
+        self.rect.x += player1_x_speed
+        if timer % 3 == 0:
+            self.walkanimationnum += 1
+            if self.walkanimationnum == 2:
+                self.walkanimationnum = 0
+        if speed == 5:
+            self.image = walkright[self.walkanimationnum]
+            self.image.set_colorkey(BLACK)
+
+#Instantiate Objects
+player1 = Player()
+
+#Set up sprite lists
+all_sprites_list = pygame.sprite.Group()
+
+#Adding Objects to sprite lists
+all_sprites_list.add(player1)
+#variables
+player1_x_speed = 0
+timer = 0
 # Loop until the user clicks the close button.
 done = False
  
@@ -28,25 +72,35 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
- 
+    
     # --- Game logic should go here
- 
-    # --- Screen-clearing code goes here
- 
-    # Here, we clear the screen to white. Don't put other drawing commands
-    # above this, or they will be erased with this command.
- 
-    # If you want a background image, replace this clear with blit'ing the
-    # background image.
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player1_x_speed = -5
+            elif event.key == pygame.K_RIGHT:
+                player1_x_speed = 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                player1_x_speed = 0
+            elif event.key == pygame.K_RIGHT:
+                player1_x_speed = 0
+
+    player1.move(player1_x_speed, timer)
+
+
     screen.fill(WHITE)
- 
+    
     # --- Drawing code should go here
- 
+    all_sprites_list.draw(screen)
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
  
     # --- Limit to 60 frames per second
     clock.tick(60)
- 
+
+    timer = timer + 1
+    if timer % 60 == 0:
+        timer = 0
+
 # Close the window and quit.
 pygame.quit()
