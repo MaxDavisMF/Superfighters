0dc5785f6aed84ab8ed154f87e9cf6e52fdaab7e
MaxDavisMF <64198116+MaxDavisMF@users.noreply.github.com>
2020-12-31 22:02:02 +0000

    Update 2 - Walking animations both ways
    The walk animations now work, although they probably need improvement. They work both ways that the player is moving, and he now faces the direction he last moved whilst still. However, currently the self.rect.x or y aren't working and can't be changed. Changed move function for players to update to be called every frame

--- Superfighters.py
+++ Superfighters.py
@@ -1,4 +1,4 @@
-#Import Libraries
+# Import Libraries
 import pygame
 import random
 import math
@@ -15,49 +15,70 @@ size = (1000, 750)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Superfighters")
 
-#Images
-player_still_image= pygame.image.load("standright_1.png").convert()
+# Images
+player_still_image_right= pygame.image.load("standright_1.png").convert()
+player_still_image_right = pygame.transform.scale(player_still_image_right, (37, 63))
+player_still_image_left = pygame.image.load("standleft_1.png").convert()
+player_still_image_left = pygame.transform.scale(player_still_image_left, (37, 63))
 player_walk_right1 = pygame.image.load("walkright_1.png").convert()
 player_walk_right1 = pygame.transform.scale(player_walk_right1, (42, 60))
-#player_walk_right2 = pygame.image.load("walkright_2.png").convert()
-#player_walk_right2 = pygame.transform.scale(player_walk_right2, (27, 60))
-player_walk_right3 = pygame.image.load("walkright_3.png").convert()
-player_walk_right3 = pygame.transform.scale(player_walk_right3, (48, 60))
-#player_walk_right4 = pygame.image.load("walkright_4.png").convert()
+player_walk_right2 = pygame.image.load("walkright_3.png").convert()
+player_walk_right2 = pygame.transform.scale(player_walk_right2, (48, 60))
+player_walk_left1 = pygame.image.load("walkleft_1.png").convert()
+player_walk_left1 = pygame.transform.scale(player_walk_left1, (42, 60))
+player_walk_left2 = pygame.image.load("walkleft_3.png").convert()
+player_walk_left2 = pygame.transform.scale(player_walk_left2, (42, 60))
 
-#create lists to cycle through for animations
-walkright = [player_walk_right1, player_walk_right3]
-#Classes
+
+# create lists to cycle through for animations
+walkright = [player_walk_right1, player_walk_right2]
+walkleft = [player_walk_left1, player_walk_left2]
+# Classes
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = player_still_image
+        self.image = player_still_image_right
         self.image.set_colorkey(BLACK)
-        self.image = pygame.transform.scale(player_still_image, (37, 63))
         self.rect = self.image.get_rect()
-        self.rect.x = 45
+        # Programe not registering self.rect. values at all
+        self.rect.x = 305
         self.rect.y = 15
         self.walkanimationnum = 0
+        self.state = "still"
+        # direction will be used to decide which image should be used when moving/ standing in different directions
+        self.direction = "right"
+        self.xspeed = 0
 
-    def move(self, speed, timer):
-        self.rect.x += player1_x_speed
-        if timer % 3 == 0:
-            self.walkanimationnum += 1
-            if self.walkanimationnum == 2:
-                self.walkanimationnum = 0
-        if speed == 5:
-            self.image = walkright[self.walkanimationnum]
-            self.image.set_colorkey(BLACK)
+    def update(self, speed, timer):
+        self.rect.x += speed
+        if speed == 5 or speed == -5:
+            if timer % 15 == 0:
+                self.walkanimationnum += 1
+                if self.walkanimationnum == 2:
+                    self.walkanimationnum = 0
+            if speed == 5:
+                self.image = walkright[self.walkanimationnum]
+            if speed == -5:
+                self.image = walkleft[self.walkanimationnum]
+        elif speed == 0:
+            if self.direction == "right":
+                self.image = player_still_image_right
+            elif self.direction == "left":
+                self.image = player_still_image_left
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect()
 
-#Instantiate Objects
+    #def fall(self):
+
+# Instantiate Objects
 player1 = Player()
 
-#Set up sprite lists
+# Set up sprite lists
 all_sprites_list = pygame.sprite.Group()
 
-#Adding Objects to sprite lists
+# Adding Objects to sprite lists
 all_sprites_list.add(player1)
-#variables
+# variables
 player1_x_speed = 0
 timer = 0
 # Loop until the user clicks the close button.
@@ -77,15 +98,20 @@ while not done:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 player1_x_speed = -5
+                player1.state = "walk"
+                player1.direction = "left"
             elif event.key == pygame.K_RIGHT:
                 player1_x_speed = 5
+                player1.state = "walk"
+                player1.direction = "right"
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 player1_x_speed = 0
+                player1.state = "still"
             elif event.key == pygame.K_RIGHT:
                 player1_x_speed = 0
-
-    player1.move(player1_x_speed, timer)
+                player1.state = "still"
+    player1.update(player1_x_speed, timer)
 
 
     screen.fill(WHITE)
