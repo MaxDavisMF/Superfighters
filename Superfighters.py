78b3a68e18ef6219cbd42a6d8dac003ab383e915
MaxDavisMF <64198116+MaxDavisMF@users.noreply.github.com>
2021-01-01 20:21:59 +0000

    Update 4 - Menu and background image
    I added a menu screen that the game starts on with the option to choose the multiplayer game mode (will add more options later). Also added background for the first map. Atm the player cannot choose the map but i will add a screen to do that once i begin to create the maps. Had difficulty centering the text for the menu screen, opted for coordinates that looked about right but will try to centre the text properly later. Currently have a glitch where the player no longer moves smoothly across the screen, and only moves each time the key is pressed down.

--- Superfighters.py
+++ Superfighters.py
@@ -16,6 +16,11 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Superfighters")
 
 # Images
+Menu_image = pygame.image.load("Home_screen_title.png").convert()
+Menu_image = pygame.transform.scale(Menu_image, (700, 150))
+background_image = pygame.image.load("citybackground.png").convert()
+background_image = pygame.transform.scale(background_image, (1000, 750))
+
 player_still_image_right = pygame.image.load("standright_1.png").convert()
 player_still_image_right = pygame.transform.scale(player_still_image_right, (37, 63))
 player_still_image_left = pygame.image.load("standleft_1.png").convert()
@@ -29,6 +34,8 @@ player_walk_left1 = pygame.transform.scale(player_walk_left1, (42, 60))
 player_walk_left2 = pygame.image.load("walkleft_3.png").convert()
 player_walk_left2 = pygame.transform.scale(player_walk_left2, (42, 60))
 
+# Fonts
+font = pygame.font.Font(None, 50)
 
 # create lists to cycle through for animations
 walkright = [player_walk_right1, player_walk_right2]
@@ -40,7 +47,6 @@ class Player(pygame.sprite.Sprite):
         self.image = player_still_image_right
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
-        # Programme not registering self.rect. values at all
         self.rect.x = 100
         self.rect.y = 100
         self.walkanimationnum = 0
@@ -67,21 +73,31 @@ class Player(pygame.sprite.Sprite):
                 self.image = player_still_image_left
         self.image.set_colorkey(BLACK)
 
-    #def fall(self):
+class Titleimage(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = Menu_image
+        self.rect = self.image.get_rect()
+        self.image.set_colorkey(BLACK)
+        self.rect.x = 150
+        self.rect.y = 25
+
+
 
 # Instantiate Objects
 player1 = Player()
-
+titlepic = Titleimage()
 # Set up sprite lists
 all_sprites_list = pygame.sprite.Group()
 
 # Adding Objects to sprite lists
-all_sprites_list.add(player1)
+# all_sprites_list.add(player1)
 # variables
 timer = 0
-# Loop until the user clicks the close button.
 done = False
- 
+Menu = True
+setup = True
+Multiplayer = False
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
  
@@ -92,38 +108,55 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
     
-    # --- Game logic should go here
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                player1.speedx = -5
-                player1.state = "walk"
-                player1.direction = "left"
-            elif event.key == pygame.K_RIGHT:
-                player1.speedx = 5
-                player1.state = "walk"
-                player1.direction = "right"
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                player1.speedx = 0
-                player1.state = "still"
-            elif event.key == pygame.K_RIGHT:
-                player1.speedx = 0
-                player1.state = "still"
-    player1.update(timer)
+        if Menu:
+            if setup:
+                setup = False
+                all_sprites_list.empty()
+                all_sprites_list.add(titlepic)
+                Multiplayertext = font.render("Press 1 to play Multiplayer!", True, WHITE)
 
-    screen.fill(WHITE)
-    
+            screen.fill(BLACK)
+            screen.blit(Multiplayertext, [275, 250])
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    Menu = False
+                    Multiplayer = True
+                    Setup = True
+        if Multiplayer:
+            if Setup:
+                all_sprites_list.empty()
+                all_sprites_list.add(player1)
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    player1.speedx = -5
+                    player1.state = "walk"
+                    player1.direction = "left"
+                elif event.key == pygame.K_RIGHT:
+                    player1.speedx = 5
+                    player1.state = "walk"
+                    player1.direction = "right"
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    player1.speedx = 0
+                    player1.state = "still"
+                elif event.key == pygame.K_RIGHT:
+                    player1.speedx = 0
+                    player1.state = "still"
+            player1.update(timer)
+
+            screen.blit(background_image, (0, 0))
+
+            # --- Limit to 60 frames per second
+            clock.tick(60)
+
+            timer = timer + 1
+            if timer % 60 == 0:
+                timer = 0
+
     # --- Drawing code should go here
     all_sprites_list.draw(screen)
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
- 
-    # --- Limit to 60 frames per second
-    clock.tick(60)
-
-    timer = timer + 1
-    if timer % 60 == 0:
-        timer = 0
-
 # Close the window and quit.
 pygame.quit()
